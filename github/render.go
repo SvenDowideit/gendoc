f0abe8d4722a0d7f59c60aa0e9d4ae2720f13e3e
Sven Dowideit <SvenDowideit@home.org.au>
2016-04-22 12:30:37 +0000

    Add support for GITHUB_TOKEN env var to get a larger API rate limit
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- github/render.go
+++ github/render.go
@@ -1,7 +1,11 @@
 package github
 
 import (
+	"log"
+	"os"
+	
 	gh "github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func Render(md string) (html string, err error) {
@@ -9,6 +13,10 @@ func Render(md string) (html string, err error) {
 	opt := &gh.MarkdownOptions{Mode: "gfm", Context: "google/go-github"}
 
 	output, _, err := client.Markdown(md, opt)
+	if _, ok := err.(*gh.RateLimitError); ok {
+		log.Println("EXITING:", err)
+		os.Exit(-1)
+	}
 	return output, err
 }
 
@@ -16,7 +24,15 @@ var ghClient *gh.Client
 
 func getClient() *gh.Client {
 	if ghClient == nil {
-		ghClient = gh.NewClient(nil)
+		if gh_token := os.Getenv("GITHUB_TOKEN"); gh_token != "" {
+			ts := oauth2.StaticTokenSource(
+				&oauth2.Token{AccessToken: gh_token},
+			)
+			tc := oauth2.NewClient(oauth2.NoContext, ts)
+			ghClient = gh.NewClient(tc)
+		} else {
+			ghClient = gh.NewClient(nil)
+		}
 	}
 	return ghClient
 }
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -14,34 +14,34 @@ func main() {
 	fileList := []string{}
 	err := filepath.Walk("./docs", func(path string, f os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Println("ERR: ", err)
+			log.Println("ERR: ", err)
 		}
 		fileList = append(fileList, path)
 		return nil
 	})
 	if err != nil {
-		fmt.Println("ERR: ", err)
+		log.Println("ERR: ", err)
 	}
 	
 	for _, file := range fileList {
-		fmt.Println("INFO: ", file)
 		if !strings.HasSuffix(file, ".md") {
-			fmt.Println("INFO: skipping non markdown file")
+			log.Println("INFO: skipping non markdown file", file)
 			continue
 		}
+		log.Println("INFO: ", file)
 		input, err := ioutil.ReadFile(file)
 		if err != nil {
-			fmt.Println("ERR: ", err)
+			log.Println("ERR: ", err)
 			continue
 		}
 		html, err := gh.Render(string(input))
 		if err != nil {
-			fmt.Println("ERR: ", err)
+			log.Println("ERR: ", err)
 			continue
 		}
 		outfile := strings.TrimSuffix(file, ".md")+".html"
 		if err = ioutil.WriteFile(outfile, []byte(html), 0644); err != nil {
-			fmt.Println("ERR: ", err)
+			log.Println("ERR: ", err)
 			continue
 		}
 	}
