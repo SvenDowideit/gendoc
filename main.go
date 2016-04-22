293892dcd8206b828caa8d3a751e55921706122b
Sven Dowideit <SvenDowideit@home.org.au>
2016-04-22 12:08:22 +0000

    convert md files in the docs dir into html fragments
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- main.go
+++ main.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	gh "github.com/SvenDowideit/gendoc/github"
 )
 
 func main() {
-	input := "# heading\n\n1. Link to issue #1\n"
-	html, err := gh.Render(input)
+	fileList := []string{}
+	err := filepath.Walk("./docs", func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println("ERR: ", err)
+		}
+		fileList = append(fileList, path)
+		return nil
+	})
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERR: ", err)
 	}
-	fmt.Println(html)
+	
+	for _, file := range fileList {
+		fmt.Println("INFO: ", file)
+		if !strings.HasSuffix(file, ".md") {
+			fmt.Println("INFO: skipping non markdown file")
+			continue
+		}
+		input, err := ioutil.ReadFile(file)
+		if err != nil {
+			fmt.Println("ERR: ", err)
+			continue
+		}
+		html, err := gh.Render(string(input))
+		if err != nil {
+			fmt.Println("ERR: ", err)
+			continue
+		}
+		outfile := strings.TrimSuffix(file, ".md")+".html"
+		if err = ioutil.WriteFile(outfile, []byte(html), 0644); err != nil {
+			fmt.Println("ERR: ", err)
+			continue
+		}
+	}
+	
 }
