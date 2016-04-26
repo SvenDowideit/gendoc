62851b6282aa319205b3acb44bab0c926601f992
Sven Dowideit <SvenDowideit@home.org.au>
2016-04-26 12:50:24 +0000

    reuse.
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- render/github.go
+++ render/github.go
@@ -11,33 +11,16 @@ import (
 	mmark "github.com/SvenDowideit/gendoc/render/mmark"
 )
 
+type renderFunc func(string) (string, error)
+
 func GithubAPI(outputDir string, markdownFiles []string) {
-	for _, file := range markdownFiles {
-		log.Println("INFO: <<", file)
-		input, err := ioutil.ReadFile(file)
-		if err != nil {
-			log.Println("ERR: ", err)
-			continue
-		}
-		// TODO: remove hugo frontmatter and store for use in the header
-		html, err := gh.Render(string(input))
-		if err != nil {
-			log.Println("ERR: ", err)
-			continue
-		}
-		outfile := filepath.FromSlash(filepath.Join(outputDir, strings.TrimSuffix(file, ".md")+".html"))
-		if err := os.MkdirAll(filepath.Dir(outfile), 0755); err != nil {
-			log.Println("ERR: ", err)
-			continue
-		}
-		if err = ioutil.WriteFile(outfile, []byte(html), 0644); err != nil {
-			log.Println("ERR: ", err)
-			continue
-		}
-		log.Println("INFO: >>", outfile)
-	}
+	processFiles(outputDir, markdownFiles, gh.Render)
 }
 func MMark(outputDir string, markdownFiles []string) {
+	processFiles(outputDir, markdownFiles, mmark.Render)
+}
+
+func processFiles(outputDir string, markdownFiles []string, render renderFunc) {
 	for _, file := range markdownFiles {
 		log.Println("INFO: <<", file)
 		input, err := ioutil.ReadFile(file)
@@ -46,7 +29,7 @@ func MMark(outputDir string, markdownFiles []string) {
 			continue
 		}
 		// TODO: remove hugo frontmatter and store for use in the header
-		html, err := mmark.Render(string(input))
+		html, err := render(string(input))
 		if err != nil {
 			log.Println("ERR: ", err)
 			continue
